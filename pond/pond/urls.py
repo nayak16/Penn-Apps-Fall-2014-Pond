0dcf6f4473e9678a7ea52ab5213ba207803b56f7
Chris <chrisbarker@cmu.edu>
2014-09-13 03:02:02 -0400

    dispatch

--- pond/pond/urls.py
+++ pond/pond/urls.py
@@ -1,12 +1,7 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'pond.views.home', name='home'),
+    url(r'^$', 'pond_app.views.home', name='home'),
     # url(r'^pond/', include('pond.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
